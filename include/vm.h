26f2fccd2bcff8a74465df409ccc066be9cafaae
Elena Parsons <jankcollector06@gmail.com>
2025-04-30 13:57:53 -0400

    Removed op_code_array_elements argument

--- include/vm.h
+++ include/vm.h
@@ -6,9 +6,7 @@
 
 /* 
     Compiles Scir Block `block` to MIPS machine code.
-    `block_op_array_elements` should be set to the number of operations.
-    `block_op_degree_array` should have the same number of elements as the number of operations.
 */
-void vmCompileScirBlock(ScirBlock *block, u16 op_code_array_elements);
+void vmCompileScirBlock(ScirBlock *block);
 
 #endif
